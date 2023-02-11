14a7933efa96a6ff48187380a7249b26a707cd9d
Tsering Lama <tsering@ad.unc.edu>
2023-02-11 11:26:20 -0500

    New stuff from Pearl Hacks! - created index.html, main.js, and style.css

---
+++ main.js
@@ -0,0 +1,10 @@
+var frog = document.getElementById("frog")
+var frog_emoji = "🐸"
+
+frog.onclick = function(){
+	if(frog.innerHTML.includes(frog_emoji)){
+        frog.innerHTML = "frog"
+	} else {
+		frog.innerHTML = frog.innerHTML + frog_emoji
+	}
+}
